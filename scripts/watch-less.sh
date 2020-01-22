8b53fd6846521e2b0c4607bbc7172823305d4cda
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-01-22 16:45:44 -0700

    Style contact form

--- scripts/watch-less.sh
+++ scripts/watch-less.sh
@@ -4,5 +4,5 @@ printf "!! This script should be run from the root of the git repository! !!\n"
 
 nix-shell --run "
     # Rebuild index.less whenever any .less file changes
-    fd -e less . less/ | entr lessc less/index.less css/index.css
+    fd -e less . less/ | entr -c lessc less/index.less css/index.css
 "
